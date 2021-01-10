f5d91051f19371ecd0b6ddb06545eba7ec66da7b
Junhao Wang <junhaowww@outlook.com>
2021-01-09 20:17:44 -0800

    add output file info

--- src/tgaimage.cpp
+++ src/tgaimage.cpp
@@ -176,6 +176,7 @@ bool TGAImage::loadRleData(std::ifstream& in)
 
 bool TGAImage::WriteTgaFile(const std::string filename, bool vFlip, bool rle) const
 {
+    spdlog::info("Output TGA File: {}", filename);
     std::uint8_t  developer_area_ref[4] = { 0, 0, 0, 0 };
     std::uint8_t  extension_area_ref[4] = { 0, 0, 0, 0 };
     std::uint8_t  footer[18] = { 'T', 'R', 'U', 'E', 'V', 'I', 'S', 'I', 'O',
